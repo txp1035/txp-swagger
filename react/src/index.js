c2d7aed8ac23c49e2869bc9758e2128a0b4c31d5
ShawDanon <shaw_danon@sina.com>
2019-01-03 11:43:28 +0800

    配置基本环境

--- react/src/index.js
+++ react/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import App from '@/views/Main';
+import '@/assets/styles/index.less';
+import 'antd/dist/antd.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
