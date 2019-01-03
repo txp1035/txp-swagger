50d7e6880df4b2e2422ac9a54cc546910d936606
DaoYuan <txp1035@gmail.com>
2019-01-03 11:43:28 +0800

    配置基本环境

---
+++ react/src/views/Main/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Button } from 'antd';
+
+export default class index extends Component {
+  render() {
+    return (
+      <div>
+        <div>123</div>
+        <Button>Default</Button>
+      </div>
+    );
+  }
+}
