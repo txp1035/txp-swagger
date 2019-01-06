b85e09e2432214e2ea55e7dfefef1bbd335f1f06
DaoYuan <txp1035@gmail.com>
2019-01-06 19:35:31 +0800

    配置路由

--- node/src/index.js
+++ node/src/index.js
@@ -1,17 +1,9 @@
 var Koa = require('koa');
-var Router = require('koa-router');
+var router = require('./routers');
 
 var app = new Koa();
-var router = new Router();
 
-router
-  .get('/', (ctx, next) => {
-    ctx.body = 'Hello World!';
-  })
-  .get('/a', (ctx, next) => {
-    ctx.body = 'a';
-  });
-
+//路由
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3000);
